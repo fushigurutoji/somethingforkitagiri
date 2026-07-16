ca7bbe0216a514ceb6827d8c63994c9060b5f0d1
Toji Fushiguru <yowhosmk@gmail.com>
2026-07-16 11:00:17 +0200

    Add files via upload

---
+++ src/data/story.js
@@ -0,0 +1,28 @@
+import img1 from "../assets/images/img1.jpg";
+import img2 from "../assets/images/img2.jpg";
+import img3 from "../assets/images/img3.jpg";
+import img4 from "../assets/images/img4.jpg";
+import img5 from "../assets/images/img5.jpg";
+
+export const story = [
+  {
+    image: img1,
+    text: "she wants the kind of love so pure and vulnerable,That when it finds her, she will feel dependable on that person who's going to stay even during the times she stop and lay. A love that speaks just enough making her feel seen, never tough",
+  },
+  {
+    image: img2,
+    text: "A love so full of glow, shine, and light, not today's kind that is unexpressive and slight.All she needs is love without fear, she wants someone to softly whisper in her ears, a little kiss whenever she's near. A love that reminds her she's truly adored",
+  },
+  {
+    image: img3,
+    text: "A love that wants to say the right things,Being present and leading her with steady wings. In the right moment she will find someone, someone's heart that is still waiting for her, to show her how. she is still here, her soul won't let go of a love that is more than it could ever show.",
+  },
+  {
+    image: img4,
+    text: "A love her heart and soul still yearns for, A lingering warmth that never closed the door.A love so real, she found in someone",
+  },
+  {
+    image: img5,
+    text: "If i'm being honest: I'd wish for her to find that someone in every dimension...universe you name it.❤️",
+  },
+];
